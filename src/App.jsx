81a44a5518d84424ad9812fc2bd74f16a8fec35f
christiantondo <christiantondo1@gmail.com>
2026-01-21 00:41:01 +0100

    ex complete and basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
-import './App.css'
+import style from "./App.module.css"
+import languages from "./data/languages"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedLang, setSelectedLang] = useState(0)
+
+  function onElementoCliccato(chi) {
+    setSelectedLang(chi);
+  }
 
   return (
     <>
+      <div className={style.container}>
+        <h1>Learn Web development</h1>
 
+        {languages.map((language, i) =>
+          <button onClick={e => onElementoCliccato(i)} key={i} className={style.btn}>{language.title}</button>
+        )}
+
+        <div>
+          <h2>{languages[selectedLang].title}</h2>
+          <p>{languages[selectedLang].description}</p>
+        </div>
+
+      </div>
     </>
   )
 }
